7380c6604a7249c703a4d55b3c858630bd08cc66
src_resources <src.res.211@gmail.com>
2024-12-10 11:39:53 +0800

    Update

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,29 @@
-#include <stdlib.h>
+#include <cstdlib>
+#include <cmath>
+#include <SFML/Window/Window.hpp>
+#include <glad/glad.h>
 
 int main() {
+    sf::Window window(sf::VideoMode(800, 600), "OpenGL");
+
+    if (!gladLoadGL()) {
+        return EXIT_FAILURE;
+    }
+
+    glEnable(GL_TEXTURE_2D);
+
+    sf::Vector2<int> winCenter = {0, 0 };
+    if (window.getSize().x <= 640) {
+        winCenter = {
+                int(std::floor(sf::VideoMode::getDesktopMode().width / 3.5)),
+                int(std::floor(sf::VideoMode::getDesktopMode().height / 4))
+        };
+    }
+    window.setPosition(winCenter);
+
+    while (window.isOpen()) {
+
+    }
+
     return EXIT_SUCCESS;
 }
